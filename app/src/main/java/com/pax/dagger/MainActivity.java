f771584c3b0bbd00122c3705aeacf04fee092f36
cc-shifo <502091250@qq.com>
2019-08-20 17:43:33 +0800

    Revert "add android Injection"
    This reverts commit c0afdeff021e6d4c6e6958c587f23913637e7e02.

--- app/src/main/java/com/pax/dagger/MainActivity.java
+++ app/src/main/java/com/pax/dagger/MainActivity.java
@@ -1,29 +1,14 @@
 package com.pax.dagger;
 
-import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-
-import com.pax.dagger.activity.SecondActivity;
-
-import dagger.android.AndroidInjection;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        AndroidInjection.inject(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button = findViewById(R.id.btn);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 }
---
+++ app/src/main/java/com/pax/dagger/java/objects/Pump.java
@@ -0,0 +1,5 @@
+package com.pax.dagger.java.objects;
+
+public interface Pump {
+  void pump();
+}
